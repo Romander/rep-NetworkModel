3564822d05dbb36ee608d2083f8dd402b874fe4f
Romander <romanderyo@gmail.com>
2016-05-22 18:40:08 +0400

    commit before(fail)

--- Microsoft Automatic Graph Layout/Microsoft Automatic Graph Layout/InputTable.cs
+++ Microsoft Automatic Graph Layout/Microsoft Automatic Graph Layout/InputTable.cs
@@ -2,15 +2,20 @@
 {
     public class InputTable
     {
+        public string Operation { get; set; }
+        public string BeforeOperations { get; set; }
+        public int Time { get; set; }
+
+        public InputTable() : this("","",-1)
+        {
+        }
+
         public InputTable(string Operation, string BeforeOperations, int Time)
         {
             this.Operation = Operation;
             this.BeforeOperations = BeforeOperations;
             this.Time = Time;
         }
-        public string Operation { get; set; }
-        public string BeforeOperations { get; set; }
-        public int Time { get; set; }
 
         public override string ToString()
         {
